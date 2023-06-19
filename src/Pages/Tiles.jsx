625ee600e7ead37767ae1f9efe2b62bd4ddc0c26
tarupathak <113367480+tarupathak@users.noreply.github.com>
2023-06-19 18:31:04 +0530

    made drawer

--- src/Pages/Tiles.jsx
+++ src/Pages/Tiles.jsx
@@ -8,11 +8,21 @@ import { Modal, ButtonToolbar, Button, Placeholder } from "rsuite";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import CarousalImg1 from "../Images/carousalImg1.svg";
+import { Drawer, IconButton } from "rsuite";
+import AngleRightIcon from "@rsuite/icons/legacy/AngleRight";
 
 const Tiles = () => {
   const [open, setOpen] = React.useState(false);
   const [show, setShow] = useState(false);
   const [size, setSize] = React.useState();
+
+  const [draw, setDraw] = React.useState(false);
+  const [placement, setPlacement] = React.useState();
+
+  const handleOpens = (key) => {
+    setDraw(true);
+    setPlacement(key);
+  };
 
   const handleShow = (value) => {
     setShow(true);
@@ -55,6 +65,60 @@ const Tiles = () => {
               <button>Gray</button>
             </div>
           </div>
+        </div>
+        <div>
+          {/*-------------Drawer--------------*/}
+          <ButtonToolbar>
+            <IconButton
+              icon={<AngleRightIcon />}
+              onClick={() => handleOpens("left")}
+            >
+              Open Options
+            </IconButton>
+          </ButtonToolbar>
+
+          <Drawer
+            id="drawer"
+            placement={placement}
+            open={draw}
+            onClose={() => setDraw(false)}
+          >
+            <Drawer.Header>
+              <Drawer.Title>Tile options</Drawer.Title>
+            </Drawer.Header>
+            <Drawer.Body>
+              <div>
+                <p className="tile-heading">Size</p>
+                <div className="draw-btn">
+                  <ButtonToolbar>
+                    <Button>16'55''</Button>
+                    <Button>16'55''</Button>
+                    <Button>16'55''</Button>
+                  </ButtonToolbar>
+                </div>
+              </div>
+              <div>
+                <p className="tile-heading">Surface Finish</p>
+                <div className="draw-btn">
+                  <ButtonToolbar>
+                    <Button>Slate</Button>
+                    <Button>Granite</Button>
+                    <Button>Marble</Button>
+                  </ButtonToolbar>
+                </div>
+              </div>
+              <div>
+                <p className="tile-heading">Colour</p>
+                <div className="draw-btn">
+                  <ButtonToolbar>
+                    <Button>Red</Button>
+                    <Button>Green</Button>
+                    <Button>Grey</Button>
+                  </ButtonToolbar>
+                </div>
+              </div>
+            </Drawer.Body>
+          </Drawer>
         </div>
         {tileItems.map((item) => {
           return (
